16f6757f1209931abfa48b29101cc5b13c504d51
André <43089870+andrebtw@users.noreply.github.com>
2022-12-09 03:06:30 +0100

    Fixed more leaks.

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,18 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 18:28:03 by anrodri2          #+#    #+#             */
-/*   Updated: 2022/12/08 06:02:04 by anrodri2         ###   ########.fr       */
+/*   Updated: 2022/12/09 03:04:53 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*back_up_string(char *s, char *saved_string)
+char	*back_up_string(char *s)
 {
 	int		i;
 	int		j;
 	char	*r_string;
-
 	i = 0;
-	free(saved_string);
 	while (s[i] && s[i] != '\n')
 		i++;
 	if (s[i])
@@ -33,21 +31,17 @@ char	*back_up_string(char *s, char *saved_string)
 		r_string[j] = s[i];
 		j++;
 		i++;
-	}	
-	free(s);
+	}
+	if (s)
+		free(s);
 	r_string[j] = '\0';
 	return (r_string);
 }
 
-int	check_if_endf(char *s, int read_value, char *temp_str, char *saved_string)
+int	check_if_endf(char *s, int read_value)
 {
 	if (!read_value && (!s || !s[0]))
-	{
-		free(s);
-		free(temp_str);
-		free(saved_string);
 		return (1);
-	}
 	return (0);	
 }
 
@@ -60,35 +54,48 @@ char	*get_next_line(int fd)
 	int			found_n;
 
 	found_n = 0;
-	if (ft_strlen_int(saved_string, 0) != ft_strlen_int(saved_string, 1))
-		found_n = 1;
 	stash = (char *) ft_calloc(1, sizeof(char));
+	temp_string = (char *) ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!saved_string)
 		saved_string = (char *) ft_calloc(1, sizeof(char));
 	if (!stash || !saved_string)
-		return (NULL);
-	if (saved_string[0])
+		return (free(stash), free(temp_string), NULL);
+	if (saved_string && saved_string[0])
+	{
 		stash = ft_strjoin(stash, saved_string);
+		if (ft_strlen_int(saved_string, 0) != ft_strlen_int(saved_string, 1))
+			found_n = 1;
+	}
 	read_output = -10;
 	while (!found_n)
 	{
+		free(temp_string);
 		temp_string = (char *) ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 		if (!temp_string)
-			return (NULL);
+			return (free(stash), NULL);
 		read_output = read(fd, temp_string, BUFFER_SIZE);
 		if (read_output == -1)
-			return (NULL);
+			return (free(temp_string), free(stash), NULL);
 		if (ft_strlen_int(temp_string, 0) != ft_strlen_int(temp_string, 1) || read_output == 0)
 			found_n = 1;
 		stash = ft_strjoin(stash, temp_string);
-		if (!found_n)
-			free(temp_string);
 	}
-	if (check_if_endf(stash, read_output, temp_string, saved_string))
+	if (check_if_endf(stash, read_output))
+	{
+		free(temp_string);
+		if (stash)
+			free(stash);
 		return (NULL);
+	}
 	if (read_output != -10)
-		saved_string = back_up_string(temp_string, saved_string);
+	{
+		free(saved_string);
+		saved_string = back_up_string(temp_string);
+	}
 	else
-		saved_string = back_up_string(saved_string, saved_string);
+	{
+		saved_string = back_up_string(saved_string);
+		free(temp_string);
+	}
 	return (stash);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 19:28:44 by anrodri2          #+#    #+#             */
-/*   Updated: 2022/12/08 05:38:15 by anrodri2         ###   ########.fr       */
+/*   Updated: 2022/12/09 02:52:40 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,20 @@ int	main(void)
 	int fd = open("test.txt", O_RDONLY);
 	char *d = get_next_line(fd);
 	char *e = get_next_line(fd);
-	/*char *f = get_next_line(fd);
-	char *h = get_next_line(fd);
+	char *f = get_next_line(fd);
+	/*char *h = get_next_line(fd);
 	char *p = get_next_line(fd);
 	char *s = get_next_line(fd);*/
 	printf("%s", d);
 	printf("%s", e);
-	/*printf("%s", f);
+	printf("%s", f);
 	/*printf("%s", h);
 	printf("%s", p);
 	printf("%s", s);*/
 
 	free(d);
 	free(e);
-	/*free(f);
+	free(f);
 	/*free(h);
 	free(p);
 	free(s);*/
