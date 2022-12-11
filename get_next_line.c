09b07755b148b5b2945f8baaf6bb25bffdeeecbe
André <gwandrewgd@gmail.com>
2022-12-11 10:25:27 +0100

    Fixed all leaks and all malloc protections.

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,18 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 18:28:03 by anrodri2          #+#    #+#             */
-/*   Updated: 2022/12/09 17:13:16 by anrodri2         ###   ########.fr       */
+/*   Updated: 2022/12/11 10:24:04 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*back_up_string(char *s)
+char	*back_up_string(char *s, int is_saved_string)
 {
 	int		i;
 	int		j;
 	char	*r_string;
+
 	i = 0;
 	while (s[i] && s[i] != '\n')
 		i++;
@@ -24,7 +25,11 @@ char	*back_up_string(char *s)
 		i++;
 	r_string = (char *) malloc (ft_strlen_int(s, 0) - ft_strlen_int(s, 1) + 1);
 	if (!r_string)
+	{
+		if (is_saved_string)
+			free(s);
 		return (NULL);
+	}
 	j = 0;
 	while (s[i])
 	{
@@ -32,8 +37,7 @@ char	*back_up_string(char *s)
 		j++;
 		i++;
 	}
-	if (s)
-		free(s);
+	free(s);
 	r_string[j] = '\0';
 	return (r_string);
 }
@@ -56,10 +60,17 @@ char	*get_next_line(int fd)
 	found_n = 0;
 	stash = (char *) ft_calloc(1, sizeof(char));
 	if (!stash)
+	{
+		if (saved_string)
+		{
+			free(saved_string);
+			saved_string = NULL;
+		}
 		return (NULL);
+	}
 	temp_string = (char *) ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!temp_string)
-		return (free(stash), NULL);
+		return (free(stash), free(saved_string), saved_string = NULL, NULL);
 	if (!saved_string)
 		saved_string = (char *) ft_calloc(1, sizeof(char));
 	if (!saved_string)
@@ -67,6 +78,8 @@ char	*get_next_line(int fd)
 	if (saved_string && saved_string[0])
 	{
 		stash = ft_strjoin(stash, saved_string);
+		if (!stash)
+			return (free(temp_string), free(saved_string), saved_string = NULL, NULL);
 		if (ft_strlen_int(saved_string, 0) != ft_strlen_int(saved_string, 1))
 			found_n = 1;
 	}
@@ -99,15 +112,15 @@ char	*get_next_line(int fd)
 	{
 		free(saved_string);
 		saved_string = NULL;
-		saved_string = back_up_string(temp_string);
+		saved_string = back_up_string(temp_string, 0);
 		if (!saved_string)
-			return (free(temp_string), NULL);
+			return (free(temp_string), free(stash), free(saved_string), saved_string = NULL, NULL);
 	}
 	else
 	{
-		saved_string = back_up_string(saved_string);
+		saved_string = back_up_string(saved_string, 1);
 		if (!saved_string)
-			return (free(temp_string), NULL);
+			return (free(temp_string), free(stash), free(saved_string), saved_string = NULL, NULL);
 		free(temp_string);
 	}
 	return (stash);
